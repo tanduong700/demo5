16e7422a7ba10e05b2a8847e0ad6b6bea81a2f59
TanDuong <tan71118@gmail.com>
2023-03-05 18:29:01 +0700

    event, listener , mail

--- app/Events/RolePermissionsChanged.php
+++ app/Events/RolePermissionsChanged.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -14,14 +15,20 @@ class RolePermissionsChanged
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $users;
+    public $roles;
+    public $permissions;
+
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $users, $roles, $permissions)
     {
-        //
+        $this->users = $users;
+        $this->roles = $roles;
+        $this->permissions = $permissions;
     }
 
     /**
